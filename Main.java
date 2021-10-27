428f3d20a5c99be906bee1278b4b8027f289f20f
Ryan Bie <30966505+ryanbie@users.noreply.github.com>
2021-10-27 17:19:51 -0400

    Update Main.java
    Completed Java Login GUI.

--- Main.java
+++ Main.java
@@ -1,12 +1,63 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Main {
+public class Main implements ActionListener {
+
+    private static JLabel userLabel;
+    private static JTextField userText;
+    private static JLabel passwordLabel;
+    private static JPasswordField userPass;
+    private static JButton button;
+    private static JLabel success;
 
     public static void main(String[] args) {
 
         JFrame frame = new JFrame(); //frame for the GUI
-        
         JPanel panel = new JPanel(); //panel for the GUI
-        
+        frame.setSize(350, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(panel);
+
+        panel.setLayout(null);
+
+        userLabel = new JLabel("Username");
+        userLabel.setBounds(10, 20, 80, 25);
+        panel.add(userLabel);
+
+        userText = new JTextField(20);
+        userText.setBounds(100, 20, 165, 25);
+        panel.add(userText);
+
+        passwordLabel = new JLabel("Password");
+        passwordLabel.setBounds(10, 50, 80, 25);
+        panel.add(passwordLabel);
+
+        userPass = new JPasswordField(20);
+        userPass.setBounds(100, 50, 165, 25);
+        panel.add(userPass);
+
+        button = new JButton("Login");
+        button.setBounds(100, 80, 80, 25);
+        button.addActionListener(new Main());
+        panel.add(button);
+
+        success = new JLabel("");
+        success.setBounds(10, 110, 300, 25);
+        panel.add(success);
+
+        frame.setVisible(true);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String user = userText.getText();
+        String password = userPass.getText();
+
+        if(user.equals("Ryan") && password.equals("fluffydinosaur123")) {
+            success.setText("Login successful!");
+        }
+
     }
 }
